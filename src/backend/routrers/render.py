f7c3e01fe8c7e34e69830d750c99f7855b024e71
undiscoveredWorld <accountssubmit@mail.ru>
2023-12-27 19:02:50 +0300

    Frontend was moved to backend

--- src/backend/routrers/render.py
+++ src/backend/routrers/render.py
@@ -12,3 +12,18 @@ router = APIRouter()
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return render_page("home.html", request)
+
+
+@router.get("/search", response_class=HTMLResponse)
+async def search(request: Request):
+    return render_page("search.html", request)
+
+
+@router.get("/anime", response_class=HTMLResponse)
+async def anime(request: Request):
+    return render_page("anime.html", request)
+
+
+@router.get("/registration", response_class=HTMLResponse)
+async def registration(request: Request):
+    return render_page("registration.html", request)
