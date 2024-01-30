67e789521a63b0196d35330b83d25cea6ea6bdc3
undiscoveredWorld <newspaw1111@mail.ru>
2024-01-30 15:09:34 +0000

    Improved code quality

--- backend/src/navigation/cruds.py
+++ backend/src/navigation/cruds.py
@@ -5,11 +5,6 @@ from common.utils import return_response_or_raise_exception
 from navigation.schemes import NavigationElement
 
 
-def get_navigation_by_id(db: Session, navigation_id: int) -> NavigationElement:
-    response = db.query(navigation.models.NavigationElement).filter_by(id=navigation_id).first()
-    return return_response_or_raise_exception(response)
-
-
 def get_all_navigation(db: Session) -> list[NavigationElement]:
     response = db.query(navigation.models.NavigationElement).all()
     return return_response_or_raise_exception(response)
